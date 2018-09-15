fac7eec9efebf64b63fef4446f04535053179222
thisisbrix901 <bmadeira7@gmail.com>
2018-09-14 20:12:11 -0400

    non-dry

---
+++ assets/javascript/game.js
@@ -0,0 +1,163 @@
+
+var audio = document.getElementById("sunnyThemeSong");
+audio.volume = 0.01;
+
+var wins = 0;
+var losses = 0;
+
+//audio pause function
+//audio.pause()
+//audio.play ?? maybe
+
+
+var currentScore = 0;
+var dennisNumber = Math.floor(Math.random() * 12 + 1);
+var macNumber = Math.floor(Math.random() * 12 + 1);
+var charlieNumber = Math.floor(Math.random() * 12 + 1);
+var deeNumber = Math.floor(Math.random() * 12 + 1);
+var frankNumber = Math.floor(Math.random() * 12 + 1);
+
+function gameInitalize() {
+    currentScore = 0;
+    dennisNumber = Math.floor(Math.random() * 12 + 1);
+    macNumber = Math.floor(Math.random() * 12 + 1);
+    charlieNumber = Math.floor(Math.random() * 12 + 1);
+    deeNumber = Math.floor(Math.random() * 12 + 1);
+    frankNumber = Math.floor(Math.random() * 12 + 1);
+    computerGeneratedNumber = Math.floor(Math.random() * 102 + 19);
+    $(".numberDisplay").text(computerGeneratedNumber);
+    $("#currentPoints").text(currentScore);
+
+}
+
+var computerGeneratedNumber = Math.floor(Math.random() * 102 + 19);
+$(".numberDisplay").text(computerGeneratedNumber);
+
+
+
+
+
+
+
+var castNames = [".dennisPic", ".macPic", ".charliePic", ".deePic", ".frankPic"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(".dennisPic").click(function () {
+    currentScore += dennisNumber
+    $("#currentPoints").text(currentScore);
+    if (computerGeneratedNumber === currentScore) {
+        alert("Nice, you hit the nail on the head");
+        wins++;
+        $(".winsDisplay").text(wins);
+        gameInitalize();
+
+    }
+    if (currentScore > computerGeneratedNumber) {
+        alert("You went too far, you lose.");
+        losses++;
+        $(".lossesDisplay").text(losses);
+        gameInitalize();
+        
+    }
+});
+
+$(".macPic").click(function () {
+    currentScore += macNumber
+    $("#currentPoints").text(currentScore);
+    if (computerGeneratedNumber === currentScore) {
+        alert("Nice, you hit the nail on the head");
+        wins++;
+        $(".winsDisplay").text(wins)
+        gameInitalize()
+    }
+    if (currentScore > computerGeneratedNumber) {
+        alert("You went too far, you lose.");
+        losses++;
+        $(".lossesDisplay").text(losses);
+        gameInitalize();
+        
+    }
+});
+
+$(".charliePic").click(function () {
+    currentScore += charlieNumber
+    $("#currentPoints").text(currentScore);
+    if (computerGeneratedNumber === currentScore) {
+        alert("Nice, you hit the nail on the head");
+        wins++;
+        $(".winsDisplay").text(wins)
+        gameInitalize()
+    }
+    if (currentScore > computerGeneratedNumber) {
+        alert("You went too far, you lose.");
+        losses++;
+        $(".lossesDisplay").text(losses);
+        gameInitalize();
+        
+    }
+
+});
+
+$(".deePic").click(function () {
+    currentScore += deeNumber
+    $("#currentPoints").text(currentScore);
+    if (computerGeneratedNumber === currentScore) {
+        alert("Nice, you hit the nail on the head");
+        wins++;
+        $(".winsDisplay").text(wins)
+        gameInitalize()
+    }
+    if (currentScore > computerGeneratedNumber) {
+        alert("You went too far, you lose.");
+        losses++;
+        $(".lossesDisplay").text(losses);
+        gameInitalize();
+        
+    }
+
+});
+
+$(".frankPic").click(function () {
+    currentScore += frankNumber
+    $("#currentPoints").text(currentScore);
+    if (computerGeneratedNumber === currentScore) {
+        alert("Nice, you hit the nail on the head");
+        wins++;
+        $(".winsDisplay").text(wins)
+        gameInitalize()
+    }
+    if (currentScore > computerGeneratedNumber) {
+        alert("You went too far, you lose.");
+        losses++;
+        $(".lossesDisplay").text(losses);
+        gameInitalize();
+        
+    }
+
+});
+
+
+$(".winsDisplay").text(wins);
+$(".lossesDisplay").text(losses);
+$("#currentPoints").text(currentScore);
+
+
